521683a8b3bfbe79ac66eb06c4f0919d81fd4eeb
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2019-03-20 23:07:43 -0400

    working navigation

--- components/navigation.jsx
+++ components/navigation.jsx
@@ -1,22 +1,66 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import '../sass/nav.scss'
 import { getPages } from '../util/navigation'
 
-const NavItem = props => {
-  const { path, title } = props.navData
+const NavLink = props => {
+  const { pages } = props
+  return pages.map((page, index) => (
+    <a href={page.path} className="nav link" key={index}>
+      {page.title}
+    </a>
+  ))
+}
+
+// const MobileNav = props => {
+//   const generateNavItems = props => {
+//     return props.map(p => <NavItem navData={p} />)
+//   }
+
+//   return <nav>{generateNavItems(pages)}</nav>
+// }
+
+// logo component
+// menu button component
+// menu nav
+
+const MenuNav = props => {
   return (
-    <a href={path} className="nav link">
-      {title}
+    <nav className="nav mobile">
+      <NavLink pages={getPages()} />
+    </nav>
+  )
+}
+
+const Logo = props => {
+  const { type } = props
+
+  return (
+    <a href={`/`}>
+      <img
+        className={`logo`}
+        src={`static/logos/logo-${type || `master`}.svg`}
+        alt={`Manny Ikomi's Logo`}
+      />
     </a>
   )
 }
 
-const MobileNav = props => {
-  const generateNavItems = props => {
-    return props.map(p => <NavItem navData={p} />)
-  }
+const MenuButton = props => {
+  const handleClick = props.click
 
-  return <nav className="mobile-nav">{generateNavItems(pages)}</nav>
+  return (
+    <button type={`button`} onClick={handleClick}>
+      Menu
+    </button>
+  )
+}
+const MenuBar = props => {
+  return (
+    <header className={`nav dock-bottom shadow`}>
+      <Logo type={`type`} />
+      {props.children || <MenuButton />}
+    </header>
+  )
 }
 
-export { MobileNav }
+export { MenuBar, MenuNav, MenuButton }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,26 +1,36 @@
 // https://nextjs.org/docs/
-import React from 'react'
+import React, { Component } from 'react'
 import HtmlHead from '../components/head'
-import { MobileNav, getPages } from '../components/navigation.jsx'
+import {
+  MobileNav,
+  MenuBar,
+  MenuNav,
+  MenuButton
+} from '../components/navigation.jsx'
 import Gallery from '../components/gallery/gallery.jsx'
 import '../sass/base.scss'
 // import { Head as NextHead } from 'next/head'
 
-const Home = props => {
-  return (
-    <React.Fragment>
-      <HtmlHead pageTitle={'Welcome! 🤓 — Manny Ikomi'} />
+class Home extends Component {
+  state = { navToggled: false }
+  handleMenuClick = e => this.setState({ navToggled: !this.state.navToggled })
 
-      {/* <MobileNav /> */}
-      <main>
-        <Gallery id={'identity'} />
-        <Gallery id="ui" />
-        <Gallery id="other" />
-      </main>
-      {/* Categorize <Gallery/> by #ui, #Identity Design, and #Other Works */}
-      {/* <Footer /> */}
-    </React.Fragment>
-  )
+  render() {
+    return (
+      <React.Fragment>
+        <HtmlHead pageTitle={'Welcome! 🤓 — Manny Ikomi'} />
+        <MenuBar>
+          <MenuButton click={this.handleMenuClick} />
+        </MenuBar>
+        {this.state.navToggled ? <MenuNav /> : null}
+        <main>
+          {/* Categorize <Gallery/> by #ui, #Identity Design, and #Other Works */}
+          <Gallery id={'identity'} />
+        </main>
+        {/* <Footer /> */}
+      </React.Fragment>
+    )
+  }
 }
 
 export default Home
