d6a84e099ad25d7d7ddc019ee8a5f8af9f308aa7
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2019-03-21 22:05:15 -0400

    start about me page

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,9 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import HtmlHead from '../components/head'
+import '../sass/base.scss'
 
 class About extends Component {
   state = {}
   render() {
-    return <div>About Page</div>
+    return (
+      <Fragment>
+        <HtmlHead pageTitle={'About Manny Ikomi'} />
+        <h1>Hi</h1>
+
+        <img src="static/headshot.jpg" alt="" />
+        <p>
+          Design thinker, lifetime learner, and crazy in love guncle. I like
+          making great things for good people. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Corporis incidunt asperiores nisi
+          voluptates. Consequatur atque dolorem non distinctio. Vero soluta
+          possimus quaerat necessitatibus neque, pariatur voluptates ipsum ipsam
+          natus sint?
+        </p>
+        <button className={`cta`}>say hello</button>
+      </Fragment>
+    )
   }
 }
 
