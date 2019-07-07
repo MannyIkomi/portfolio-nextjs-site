55204e53d8e34ab6fd2d8f2214d96531fd86b038
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2019-07-07 10:50:01 -0400

    add google analytics tag

--- components/head.jsx
+++ components/head.jsx
@@ -14,6 +14,17 @@ const HtmlHead = props => {
         name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no"
       />
+      
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-143395225-1"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-143395225-1');
+      </script>
+      
     </Head>
   )
 }
