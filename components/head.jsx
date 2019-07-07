a1af2e7d0e1534b20728e682871f477985017d24
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2019-07-07 10:56:55 -0400

    fix google analytics tag syntax

--- components/head.jsx
+++ components/head.jsx
@@ -6,6 +6,16 @@ const HtmlHead = props => {
   const { pageTitle, description } = props
   return (
     <Head>
+      {/* Global site tag (gtag.js) - Google Analytics */}
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=UA-143395225-1"
+      />
+      <script>
+        window.dataLayer = window.dataLayer || []; function gtag()
+        {dataLayer.push(arguments)}; gtag('js', new Date()); gtag('config',
+        'UA-143395225-1');
+      </script>
       <link rel="stylesheet" href="https://use.typekit.net/esl0nbd.css" />
       <title>{pageTitle}</title>
       <meta name="description" content={description} />
@@ -14,17 +24,6 @@ const HtmlHead = props => {
         name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no"
       />
-      
-      {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-143395225-1"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-143395225-1');
-      </script>
-      
     </Head>
   )
 }
