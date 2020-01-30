2fe2581ccf9ef4a337e019216cf5f34a67653c33
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2020-01-29 22:12:15 -0500

    add gtag plugin for google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,17 @@ module.exports = {
     author: `Manny Ikomi`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: `UA-143395225-1`,
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    },
     `gatsby-plugin-emotion`,
     `gatsby-plugin-react-helmet`,
     {
