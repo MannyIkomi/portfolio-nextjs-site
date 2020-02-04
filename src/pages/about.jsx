6946c965048e3945d5a3e0405d7fe7719063a341
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2020-02-03 20:57:03 -0500

    intentional inspirations sequential order

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -216,7 +216,7 @@ export const query = graphql`
         }
       }
     }
-    allStrapiInspiration {
+    allStrapiInspiration(sort: { fields: sequence }) {
       nodes {
         id
         website
