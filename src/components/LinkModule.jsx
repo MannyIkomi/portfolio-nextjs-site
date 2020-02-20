0af4244a53343cf946780e8b274d6635107f4b5c
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2020-02-20 00:30:01 -0500

    Create LinkModule.jsx

---
+++ src/components/LinkModule.jsx
@@ -0,0 +1,73 @@
+/** @jsx jsx */
+import React, { Fragment } from "react"
+import { css, jsx } from "@emotion/core"
+
+import { moduleProps } from "../util/props"
+import {
+  SANS_HEADING,
+  styleTransition,
+  typesetInteraction,
+  colors,
+  moduleContainer,
+} from "../styles"
+
+export const LinkModule = props => {
+  const { image, imageAlt, imageFill, url, type, text, id } = props
+  console.table(props)
+
+  const isExternalLink = true
+  // https://www.gatsbyjs.org/docs/gatsby-link/#reminder-use-link-only-for-internal-links
+  // ensure local path links used Gatsby Link component
+  if (!url) {
+    throw new Error("The LinkModule requires a URL")
+  }
+
+  return (
+    <a
+      css={{
+        ...moduleContainer(),
+        ...styleTransition(),
+        ...typesetInteraction({
+          ":hover": {
+            color: colors.orange,
+            backgroundColor: colors.muteGray,
+            borderBottom: `0.25rem solid ${colors.orange}`,
+          },
+        }),
+        padding: "1rem",
+        ...SANS_HEADING,
+        fontWeight: "bold",
+        fontSize: "2rem",
+
+        color: colors.muteGray,
+        backgroundColor: colors.orange,
+        borderBottom: `0.25rem solid ${colors.muteGray}`,
+      }}
+      title={text}
+      href={url}
+    >
+      {text}
+    </a>
+  )
+}
+
+// <Button>
+
+// </Button>
+// <figure
+//   css={[
+//     moduleContainer(),
+//     {
+//       // how to make container resizeable?
+//       display: "block",
+//       width: "100%",
+//       maxHeight: "90vh",
+//       height: "90vh",
+//       overflow: "auto",
+//     },
+//   ]}
+//   dangerouslySetInnerHTML={{ __html: text }}
+// >
+//   {/* <iframe title={text} src={url} frameBorder="0"></iframe> */}
+// </figure>
+LinkModule.propTypes = moduleProps
