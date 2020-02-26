fa430e506e01a5d0f7333c6c9c785514ac3380c6
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2020-02-25 20:14:00 -0500

    Create ProjectTagHeading.jsx

---
+++ src/pages/ProjectTagHeading.jsx
@@ -0,0 +1,10 @@
+/** @jsx jsx */
+import React, from "react"
+import { jsx } from "@emotion/core"
+import { colors } from "../styles";
+
+export const ProjectTagHeading = ({ children, ...restProps }) => {
+  return (<h1 css={{ textAlign: "center", marginBottom: "1rem", color: colors.orange }} {...restProps}>
+    {children}
+  </h1>);
+};
