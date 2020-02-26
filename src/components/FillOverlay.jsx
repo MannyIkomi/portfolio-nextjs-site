82cd093c9b30097044b44413a44d6266234aa301
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2020-02-25 20:15:03 -0500

    extract Motif to component

--- src/components/FillOverlay.jsx
+++ src/components/FillOverlay.jsx
@@ -21,32 +21,6 @@ const positionOverlay = override =>
     ...override,
   })
 
-export const Motif = props => (
-  <div css={{ fill: "black" }} {...props}>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 73.34912 33.82959">
-      <defs>
-        {/* <style>.a2b08d38-d6e8-4c4f-880f-604fe16a7b6e{fill:#262626;}</style> */}
-      </defs>
-      <title>motif-gallery</title>
-      <g id="aa6f012a-c32a-4f97-a45a-572761dcb288" data-name="Layer 2">
-        <g id="bdb8b8fa-c1cf-4191-aa76-6e3c3c34d75e" data-name="Layer 1">
-          <rect
-            class="a2b08d38-d6e8-4c4f-880f-604fe16a7b6e"
-            x="56.67725"
-            y="28.99683"
-            width="16.67188"
-            height="4.83276"
-          />
-          <polygon
-            class="a2b08d38-d6e8-4c4f-880f-604fe16a7b6e"
-            points="0 0 0 4.833 5.195 4.833 5.195 9.666 42.88 9.666 42.88 14.499 19.791 14.499 19.791 19.331 26.397 19.331 26.397 24.164 73.349 24.164 73.349 19.331 73.349 14.499 73.349 9.666 73.349 4.833 73.349 0 0 0"
-          />
-        </g>
-      </g>
-    </svg>
-  </div>
-)
-
 export const ColorOverprint = ({ children, ...restProps }) => (
   <div css={[positionOverlay(), { mixBlendMode: "multiply" }]} {...restProps}>
     {children}
