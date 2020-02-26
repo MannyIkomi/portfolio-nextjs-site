25d8aaa9d3bf4ea5040397d677b6a31a5914f992
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2020-02-25 20:14:36 -0500

    Create Motif.jsx
    create seperate components for motifs as inline SVG's

---
+++ src/components/Motif.jsx
@@ -0,0 +1,63 @@
+import React from "react"
+import { colors } from "../styles"
+
+export const MotifRight = props => (
+  <svg
+    css={{ fill: colors.darkGray }}
+    {...props}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 73.34912 33.82959"
+  >
+    <title>motif-right</title>
+    <g id="aa6f012a-c32a-4f97-a45a-572761dcb288" data-name="Layer 2">
+      <g id="bdb8b8fa-c1cf-4191-aa76-6e3c3c34d75e" data-name="Layer 1">
+        <rect
+          class="a2b08d38-d6e8-4c4f-880f-604fe16a7b6e"
+          x="56.67725"
+          y="28.99683"
+          width="16.67188"
+          height="4.83276"
+        />
+        <polygon
+          class="a2b08d38-d6e8-4c4f-880f-604fe16a7b6e"
+          points="0 0 0 4.833 5.195 4.833 5.195 9.666 42.88 9.666 42.88 14.499 19.791 14.499 19.791 19.331 26.397 19.331 26.397 24.164 73.349 24.164 73.349 19.331 73.349 14.499 73.349 9.666 73.349 4.833 73.349 0 0 0"
+        />
+      </g>
+    </g>
+  </svg>
+)
+
+export const MotifLeft = props => (
+  <svg
+    css={{ fill: colors.darkGray }}
+    {...props}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 80.90186 33.82959"
+  >
+    <title>motif-left</title>
+    <g id="Layer_2" data-name="Layer 2">
+      <g id="Layer_1-2" data-name="Layer 1">
+        <polygon
+          class="cls-1"
+          points="33.95 24.164 33.95 19.331 27.344 19.331 27.344 14.498 50.433 14.498 50.433 9.666 12.748 9.666 12.748 4.833 7.553 4.833 7.553 0 0 0 0 4.833 0 9.666 0 14.498 0 19.331 0 24.164 0 28.997 0 33.83 64.23 33.83 64.23 28.997 80.902 28.997 80.902 24.164 33.95 24.164"
+        />
+      </g>
+    </g>
+  </svg>
+)
+
+export const MotifNegative = props => (
+  <div css={{ fill: colors.darkGray }} {...props}>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80.90186 62.82642">
+      <title>favicon</title>
+      <g id="Layer_2" data-name="Layer 2">
+        <g id="Layer_1-2" data-name="Layer 1">
+          <polygon
+            class="cls-1"
+            points="33.95 19.331 27.344 19.331 27.344 14.499 50.433 14.499 50.433 9.666 12.748 9.666 12.748 4.833 7.552 4.833 7.552 0 0 0 0 38.662 7.552 38.662 7.552 43.495 12.748 43.495 12.748 48.328 50.433 48.328 50.433 53.161 27.344 53.161 27.344 57.994 33.95 57.994 33.95 62.826 80.902 62.826 80.902 33.83 64.229 33.83 64.229 28.997 80.902 28.997 80.902 24.164 33.95 24.164 33.95 19.331"
+          />
+        </g>
+      </g>
+    </svg>
+  </div>
+)
