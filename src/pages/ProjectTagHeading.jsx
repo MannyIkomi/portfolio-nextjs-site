383c800e3a826a30957424013e0c28a8f9c3a2ac
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2020-02-25 20:16:06 -0500

    Update ProjectTagHeading.jsx

--- src/pages/ProjectTagHeading.jsx
+++ src/pages/ProjectTagHeading.jsx
@@ -1,10 +1,15 @@
 /** @jsx jsx */
-import React, from "react"
+import React from "react"
 import { jsx } from "@emotion/core"
-import { colors } from "../styles";
+import { colors } from "../styles"
 
 export const ProjectTagHeading = ({ children, ...restProps }) => {
-  return (<h1 css={{ textAlign: "center", marginBottom: "1rem", color: colors.orange }} {...restProps}>
-    {children}
-  </h1>);
-};
+  return (
+    <h1
+      css={{ textAlign: "center", marginBottom: "1rem", color: colors.orange }}
+      {...restProps}
+    >
+      {children}
+    </h1>
+  )
+}
