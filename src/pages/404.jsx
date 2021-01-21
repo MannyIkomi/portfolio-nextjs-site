e532e27449bb90a8a9b2857f2d9ab95a22c437e1
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2021-01-21 16:42:57 -0500

    basic 404 page with link back home

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -2,12 +2,34 @@ import React from "react"
 
 import Layout from "../components/layout"
 import HtmlHead from "../components/HtmlHead"
+import { TypesetLink } from "../components/TypesetLink"
+import { colors, flex, SANS_TYPE } from "../styles"
+import { fon } from "../styles/index"
 
 const NotFoundPage = () => (
   <Layout>
     <HtmlHead title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn't exist... the sadness.</p>
+    <section
+      css={{
+        textAlign: "center",
+
+        ...flex("column"),
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        width: "100vw",
+
+        background: colors.darkGray10,
+      }}
+    >
+      <h1 css={{ textAlign: "center", fontSize: "50vmin", ...SANS_TYPE }}>
+        404
+      </h1>
+      <p>
+        Sorry, that page doesn't exist! <br />
+        <TypesetLink to={"/"}>Take me home!</TypesetLink>
+      </p>
+    </section>
   </Layout>
 )
 
