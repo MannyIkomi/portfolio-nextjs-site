6f295029a843fee12067169926820a4f0b1d959e
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2021-02-15 22:36:05 -0500

    max reading width

--- src/components/ContentArea.jsx
+++ src/components/ContentArea.jsx
@@ -1,11 +1,11 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core"
 import React from "react"
-import { maxLineMeasure } from "../styles"
+import { maxReadingWidth } from "../styles"
 export const ContentArea = props => {
   const { children, ...rest } = props
   return (
-    <div className={"content"} css={{ ...maxLineMeasure }} {...rest}>
+    <div className={"content"} css={{ ...maxReadingWidth }} {...rest}>
       {children}
     </div>
   )
