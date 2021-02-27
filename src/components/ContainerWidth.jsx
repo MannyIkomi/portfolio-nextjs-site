38c9ed02fcb6b28ab0856e7632ad569ae633f70e
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2021-02-27 14:33:42 -0500

    refactor Con

--- src/components/ContainerWidth.jsx
+++ src/components/ContainerWidth.jsx
@@ -1,12 +1,16 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core"
 import React from "react"
-import { MAX_CONTAINER_WIDTH, MAX_TEXT_WIDTH } from "../styles"
+import { maxContainerWidth, maxTypeWidth } from "../styles"
 
 export const ContainerWidth = props => {
   const { children, ...rest } = props
   return (
-    <div className={"content"} css={{ ...MAX_CONTAINER_WIDTH }} {...rest}>
+    <div
+      className={"container"}
+      css={{ width: "100%", ...maxContainerWidth }}
+      {...rest}
+    >
       {children}
     </div>
   )
