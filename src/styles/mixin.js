26598d06d839f4003aa1cac81c0d8d85587ef05a
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2021-02-27 15:00:17 -0500

    refactor grid styles and mixins

--- src/styles/mixin.js
+++ src/styles/mixin.js
@@ -1,9 +1,8 @@
-import {
-  tabletMediaQuery,
-  colors,
-  typography,
-  desktopMediaQuery,
-} from "./index"
+export const DESKTOP_VIEWPORT = `1200px`
+export const TABLET_VIEWPORT = `700px`
+
+export const tabletMediaQuery = `@media screen and (min-width: ${TABLET_VIEWPORT})`
+export const desktopMediaQuery = `@media screen and (min-width: ${DESKTOP_VIEWPORT})`
 
 export const onMedia = (query = "", styles = {}) => ({
   [`@media (${query})`]: {
@@ -19,8 +18,7 @@ export const onSupport = (query = "", styles = {}) => ({
   },
 })
 
-export const supportsGrid = (hasSupport = {}) => (onSupport(`display: grid`, {display: 'grid', ...hasSupport}))
-
+export const supportsGrid = (hasSupport = {}) => (onSupport(`display: grid`, hasSupport))
 
 export const onTabletMedia = (tabletStyles = {}) => ({
   [tabletMediaQuery]: {
@@ -39,10 +37,25 @@ export const flex = (direction = "column") => ({
   flexDirection: direction,
 })
 
+export const grid = (styles = {}) => supportsGrid({
+    display: 'grid',
+    ...styles
+  })
+
+
 export const onMediaWidth = (width = "0px", styles = {}) => ({
   [`@media screen and (min-width: ${width})`]: {
     ...styles,
   },
 })
+
+export const grid12Columns = (styles = {}) => supportsGrid(
+    grid({
+      gridTemplateColumns: "repeat(12, 1fr)",
+      gridGap: "1rem",
+      ...styles
+    })
+)
+
 
 export const positionSticky = (top = 0) => ({ position: "sticky", top })
