7f1a3b4c169e4dec80f0fe0dd95a72b2180ec578
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2021-02-27 15:01:41 -0500

    move global styles to it's own file

--- src/styles/index.js
+++ src/styles/index.js
@@ -58,7 +58,6 @@ export const ulStyles = {
       content: `"::"`
     }
   }
-
 }
 
 export const aspectRatioLetter = {
@@ -78,120 +77,14 @@ export const h1Text = override => ({
   }),
 })
 
-
 export const linkActive = (override = {}) => ({
   color: colors.TURQUOISE,
   ...override,
 })
 
-export const GLOBAL = {
-  "*": {
-    boxSizing: "border-box",
-  },
-  ":root": {
-    // fontSize: "100%",
-    scrollBehavior: "smooth",
-    ...SANS_TYPE,
-  },
-  main: {
-    width: "100%",
-  },
-  // body: {
-  //   overflow: "hidden",
-  //   overflowX: "hidden",
-  // },
-  a: {
-    // https://stackoverflow.com/questions/1823341/how-do-i-get-this-css-text-decoration-override-to-work
-    // setting text decoration as an inherited property cannot be overrided due to user agent implementations
-    // best to clear styling an re-apply using composition
-    color: "inherit",
-    textDecoration: `none`,
-    // textDecoration: `underline solid ${colors.orange}`,
-    // textDecorationColor: colors.orange,
-    // textDecorationStyle: "solid",
-
-    "&:hover": {
-      color: colors.YELLOW,
-    },
-  },
-  button: {
-    display: "inline-block",
-    padding: 0,
-    margin: 0,
-    border: "none",
-    outline: "none",
-
-    cursor: "pointer",
-
-    textAlign: "center",
-    textDecoration: "none",
-  },
-
-  "h1, h2, h3, h4, h5": {
-    lineHeight: 1.2,
-    ...fontSizeTransition,
-    
-    ...CODE_TYPE,
-    textTransform: "lowercase",
-    fontWeight: "normal",
-  },
-  "p, span, ul, ol, button": {
-    ...SANS_TYPE,
-    fontWeight: 300,
-    
-    lineHeight: 1.5,
-    listStyle: "square",
-    fontVariantNumeric: "proportional-nums",
-  },
-
-  h1: {
-    fontSize: "2rem",
-    color: colors.TURQUOISE,
-  },
-  h2: {
-    fontSize: "1.5rem",
-  },
-  h3: {
-    fontSize: "1.333rem",
-  },
-  h4: {
-    fontSize: "1.25rem",
-  },
-
-  ...onTabletMedia({
-    h1: {
-      fontSize: "2.5rem",
-    },
-    h2: {
-      fontSize: "2rem",
-    },
-    h3: {
-      fontSize: "1.5rem",
-    },
-    h4: {
-      fontSize: "1.3333rem",
-    },
-  }),
-
-  ...onDesktopMedia({
-    h1: {
-      fontSize: "3rem",
-    },
-    h2: {
-      fontSize: "2.25rem",
-    },
-    h3: {
-      fontSize: "1.75rem",
-    },
-    h4: {
-      fontSize: "1.5rem",
-    },
-  }),
-}
-
+export * from './globalStyles'
 export * from "./colors"
 export * from "./mixin"
 export * from "./reset"
 export * from './typography'
-// export * from './moduleContainer'
-// export * from './GlobalStyles'
+
