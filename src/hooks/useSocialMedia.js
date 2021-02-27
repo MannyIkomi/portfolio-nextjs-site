73187e1a7fc7944f4e5c66dfedf56551deaba655
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2021-02-27 14:58:04 -0500

    order social media graph data

--- src/hooks/useSocialMedia.js
+++ src/hooks/useSocialMedia.js
@@ -3,13 +3,13 @@ import { graphql, useStaticQuery } from "gatsby"
 export const useSocialMedia = () => {
   const data = useStaticQuery(graphql`
     query SocialMedia {
-      allStrapiSocials {
-        nodes {
-          href
-          alt
-          platform
-        }
-      }
+      allStrapiSocials(sort: {order: DESC, fields: id}) {
+    nodes {
+      href
+      alt
+      platform
+    }
+  }
     }
   `)
   const socialMedia = data.allStrapiSocials.nodes
