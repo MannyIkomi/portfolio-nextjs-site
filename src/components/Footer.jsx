35e4b28d89ba364f7502037ef88f82eadbabe821
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2021-08-22 17:38:15 -0400

    remove grid styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,23 +23,19 @@ import { MENU_LINKS } from "../util/menuLinks"
 import { ContainerWidth } from "./ContainerWidth"
 
 export const Footer = props => {
-  const socialMedia = useSocialMedia()
+  const socials = useSocialMedia()
   return (
     <footer
       css={{
-        backgroundColor: colors.NAVY_BLUE,
-        color: colors.LIGHT_GRAY,
+        backgroundColor: colors.LIGHT_GRAY_FOREGROUND,
+        color: colors.PRIMARY,
         fontFamily: typography.SANS_TYPE,
         padding: "1rem",
-        ...onTabletMedia({
-          padding: TOUCH_TARGET,
-        }),
       }}
     >
       <div
         css={{
-          position: "relative",
-          minHeight: "100vh",
+          // minHeight: "100vh",
           ...maxContainerWidth,
           margin: "auto",
 
@@ -53,136 +49,45 @@ export const Footer = props => {
           p: {
             fontFamily: typography.SANS_TYPE,
           },
-
-          ...grid12Columns({
-            gridTemplateRows: `[content] auto [copyright] min-content`,
-            alignItems: "end",
-          }),
-
-          ...onTabletMedia({
-            minHeight: "50vh",
-          }),
         }}
       >
         <LogoMaster
-          fill={colors.LIGHT_GRAY}
-          stroke={colors.YELLOW}
+          fill={colors.PRIMARY}
+          stroke={colors.ACCENT}
           css={{
             minWidth: TOUCH_TARGET,
             width: "50%",
             maxWidth: "10rem",
             margin: "1rem 0",
-            ...supportsGrid({
-              gridColumn: "2 / -1",
-              ...onTabletMedia({
-                width: "100%",
-                gridRow: 1,
-                gridColumn: "1 / 3",
-                margin: 0,
-                marginBottom: "-1rem", //aligns baseline with links
-              }),
-            }),
           }}
         />
 
-        <div
-          css={[
-            {
-              ...flex("row"),
-              alignItems: "center",
-              justifyContent: "flex-start",
-            },
-            supportsGrid({
-              gridColumn: "2 / 6",
-              gridRow: 2,
-              ...onTabletMedia({
-                gridRow: 1,
-                gridColumn: "-1 / 10 ",
-                justifyContent: "flex-end",
-              }),
-            }),
-          ]}
-        >
-          {/* {socialMedia.map(social => (
-            <SocialIcon
-              key={social.platform}
-              {...social}
-              css={{
-                svg: { fill: colors.YELLOW },
-                marginRight: "1rem",
-                maxWidth: "2rem",
-                ...supportsGrid(
-                  onTabletMedia({ marginLeft: "1rem", marginRight: 0 })
-                ),
-              }}
-            />
-          ))} */}
-        </div>
         <ul
           css={[
-            flex("column"),
             {
-              listStyle: "none",
-              alignItems: "flex-start",
+              ...flex("column"),
+              padding: 0,
+              li: { listStyle: "none", "::marker": { content: '""' } },
             },
-            supportsGrid({
-              gridColumn: "2 / 6",
-
-              ...onTabletMedia({
-                gridRow: 1,
-                gridColumn: "4 / 8",
-                ...flex("row"),
-              }),
-            }),
           ]}
         >
-          {MENU_LINKS.map(([label, to]) => (
-            <li key={label}>
-              <TypesetLink
-                href={to}
-                css={[
-                  {
-                    padding: "1rem 0",
-                    textTransform: "uppercase",
-                    fontWeight: 600,
-                    whiteSpace: "nowrap",
-                    textDecorationColor: colors.YELLOW,
-                  },
-                  onTabletMedia({
-                    padding: 0,
-                    marginRight: "1rem",
-                  }),
-                ]}
-              >
-                {label}
-              </TypesetLink>
-            </li>
-          ))}
+          {socials.map(platform => {
+            return (
+              <li>
+                <SocialIcon {...platform} />
+              </li>
+            )
+          })}
         </ul>
 
-        <ContainerWidth
-          css={{
-            alignSelf: "flex-end",
-            ...supportsGrid({
-              gridColumn: "1 / -1",
-            }),
-          }}
-        >
+        <ContainerWidth>
           <p
             css={{
               ...CODE_TYPE,
-              textAlign: "right",
               fontSize: "0.75rem",
               textTransform: "uppercase",
 
-              opacity: 0.5,
-
-              ...onTabletMedia({
-                ...supportsGrid({
-                  marginTop: TOUCH_TARGET,
-                  textAlign: "left",
-                }),
-              }),
+              opacity: 0.75,
             }}
           >
             <small>
