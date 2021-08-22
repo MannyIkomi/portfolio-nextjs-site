0341ce102240d6f078c7810fa55db4b219bd2e88
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2021-08-22 13:01:36 -0400

    remove old grid styles

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -14,27 +14,14 @@ import {
 export const ProjectList = ({ children, css, ...props }) => {
   return (
     <ul
-      className={"gallery"}
       css={{
         listStyle: "none",
-        // backgroundColor: colors.darkGray,
-        ...flex("column"),
-        alignItems: "center",
-        width: "100%",
         padding: 0,
         li: {
           "::marker": {
             content: "initial",
           },
         },
-        ...onTabletMedia(
-          grid({
-            // padding: TOUCH_TARGET,
-            gridTemplateColumns: "1fr 1fr",
-            gridGap: `2rem`,
-            gridAutoFlow: "row",
-          })
-        ),
       }}
       {...props}
     >
