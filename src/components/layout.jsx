a4caae86e35484577e2700689be04d2d03a9c518
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2021-09-05 14:13:47 -0400

    fix static query bug
    Error in function useStaticQuery in ./.cache/gatsby-browser-entry.js:77
    
    Removed unused static queries from root layout components

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -12,20 +12,8 @@ import PropTypes from "prop-types"
 import { typography, colors, RESET } from "../styles"
 import { GLOBAL } from "../styles/globalStyles"
 import { useStaticQuery, graphql } from "gatsby"
-import { Footer } from "./Footer"
-import Header from "./Header"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
       <Global
