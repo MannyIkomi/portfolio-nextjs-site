529fb1c10bd95c84f71aa44447cd952d33b4bccc
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2022-05-28 15:54:00 -0400

    create new parsers for Notion block content

--- src/templates/ProjectTemplate.jsx
+++ src/templates/ProjectTemplate.jsx
@@ -26,59 +26,71 @@ import { StickyScrollContainer } from "../components/StickyScrollContainer"
 import { StickyMenuBar } from "../components/StickyMenuBar"
 import { ContainerWidth } from "../components/ContainerWidth"
 import { Footer } from "../components/Footer"
-import { ActionLinkSlice } from "../components/slices/ActionLinkSlice"
 
 // Refactor Modules into Prismic Slices
-import { ImageSlice } from "../components/slices/ImageSlice"
+import { ImageBlock } from "../components/blocks/ImageBlock"
 import {
-  RichContentSlice,
+  ParagraphBlock,
   switchRichContentToComponent,
-} from "../components/slices/RichContentSlice"
+} from "../components/blocks/ParagraphBlock"
 
 import { TokenList } from "../components/TokenList"
+import {
+  TOGGLE,
+  HEADING_1,
+  HEADING_2,
+  DIVIDER,
+  HEADING_3,
+  IMAGE,
+  PARAGRAPH,
+  QUOTE,
+} from "../util/blockTypes"
 
 import {
-  ACCORDIAN,
-  FULL_IMAGE,
-  IMAGE_GALLERY,
-  INLINE_IMAGE,
-  ACTION_LINK,
-  RICH_TEXT,
-} from "../util/sliceTypes"
-import { AccordianSlice } from "../components/slices/AccordianSlice"
-import ImageGallerySlice from "../components/slices/ImageGallery"
-import { DebugDataPre } from "../components/DebugDataPre"
+  Heading1Block,
+  Heading2Block,
+  Heading3Block,
+} from "../components/blocks/HeadingBlock"
 
-function switchSliceToComponent(slice) {
-  switch (slice.slice_type || slice.type) {
-    case FULL_IMAGE:
-    case INLINE_IMAGE:
-      return <ImageSlice {...slice} key={slice.id} />
-    case ACCORDIAN:
-      return <AccordianSlice {...slice} key={slice.id} />
-    case IMAGE_GALLERY:
-      return <ImageGallerySlice {...slice} key={slice.id} />
-    case ACTION_LINK:
-      if (slice.primary.isBroken) {
-        console.warn(
-          `"${slice.primary.label}" link is broken, the link will not be rendered`,
-          slice
-        )
-        return
-      }
-      return <ActionLinkSlice {...slice} key={slice.id} />
-    case RICH_TEXT:
-      return <RichContentSlice {...slice} key={slice.id} />
+import { DividerBlock } from "../components/blocks/DividerBlock"
+import { QuoteBlock } from "../components/blocks/QuoteBlock"
+
+export function parseRichText(props) {
+  const { type } = props
+  const block = props[type]
+  // TODO: Add support for annotations like bold, italic, underline
+  return block.rich_text.map(ast => <span>{ast.text.content}</span>)
+}
+
+export function parseCaption(props) {
+  const { type } = props
+  const block = props[type]
+  return block.caption.map(ast => ast.text.content).join(" ")
+}
+
+function switchBlockToComponent(block) {
+  switch (block.type) {
+    case HEADING_1:
+      return <Heading1Block {...block} key={block.id}></Heading1Block>
+    case HEADING_2:
+      return <Heading2Block {...block} key={block.id}></Heading2Block>
+    case HEADING_3:
+      return <Heading3Block {...block} key={block.id}></Heading3Block>
+
+    case IMAGE:
+      return <ImageBlock {...block} key={block.id} />
+    case DIVIDER:
+      return <DividerBlock {...block} />
+    case QUOTE:
+      return <QuoteBlock {...block} />
+    case PARAGRAPH:
+      return <ParagraphBlock {...block} key={block.id} />
 
     default:
-      console.warn(
-        `Could not find matching Component for Slice: ${slice.slice_type ||
-          slice.type}`
-      )
+      console.warn(`Could not find matching Component for Block: ${block.type}`)
       return (
         <div css={{ backgroundColor: "red" }}>
-          Could not find matching Component for Slice:{" "}
-          {slice.slice_type || slice.type}
+          Could not find matching Component for Slice: {block.type}
         </div>
       )
   }
@@ -90,7 +102,6 @@ const ProjectTemplate = ({ data, site }) => {
 
   const { Role, Status, Name } = properties
   const title = Name.title[0].plain_text
-  console.log(properties)
 
   return (
     <Layout>
@@ -139,8 +150,7 @@ const ProjectTemplate = ({ data, site }) => {
             <div
               css={[{ ...flex("column"), alignItems: "center", width: "100%" }]}
             >
-              <DebugDataPre>{children}</DebugDataPre>
-              {/* {body.map(switchSliceToComponent)} */}
+              {children.map(switchBlockToComponent)}
             </div>
           </article>
         </main>
@@ -208,6 +218,24 @@ export const query = graphql`
         ... on NotionBlock_toggle {
           type
           notionId
+          toggle {
+            color
+            rich_text {
+              annotations {
+                bold
+                code
+                color
+                italic
+                strikethrough
+                underline
+              }
+              plain_text
+              text {
+                content
+              }
+              type
+            }
+          }
         }
         ... on NotionBlock_table_of_contents {
           id
@@ -220,24 +248,139 @@ export const query = graphql`
         ... on NotionBlock_quote {
           id
           type
+          quote {
+            rich_text {
+              annotations {
+                bold
+                code
+                color
+                italic
+                strikethrough
+                underline
+              }
+              plain_text
+              type
+              text {
+                content
+              }
+            }
+          }
         }
         ... on NotionBlock_paragraph {
           id
           type
+          paragraph {
+            rich_text {
+              href
+              plain_text
+              type
+              text {
+                content
+              }
+            }
+          }
         }
         ... on NotionBlock_numbered_list_item {
           id
           type
+          numbered_list_item {
+            rich_text {
+              plain_text
+              type
+              text {
+                content
+              }
+            }
+          }
         }
         ... on NotionBlock_image {
           id
           type
+          image {
+            caption {
+              plain_text
+              text {
+                content
+              }
+              type
+            }
+            file {
+              url
+            }
+            external {
+              url
+            }
+          }
         }
         ... on NotionBlock_heading_3 {
           id
           type
+          heading_3 {
+            rich_text {
+              plain_text
+              type
+              text {
+                content
+              }
+            }
+          }
         }
         ... on NotionBlock_heading_2 {
+          id
+          type
+          heading_2 {
+            rich_text {
+              plain_text
+              text {
+                content
+              }
+              type
+            }
+          }
+        }
+        ... on NotionBlock_heading_1 {
+          id
+          type
+          heading_1 {
+            rich_text {
+              plain_text
+              type
+              text {
+                content
+              }
+            }
+          }
+        }
+        ... on NotionBlock_divider {
+          type
+          id
+        }
+        ... on NotionBlock_callout {
+          id
+          type
+        }
+        ... on NotionBlock_bulleted_list_item {
+          id
+          type
+          bulleted_list_item {
+            rich_text {
+              plain_text
+              annotations {
+                bold
+                code
+                color
+                italic
+                strikethrough
+                underline
+              }
+              text {
+                content
+              }
+              type
+            }
+          }
+        }
+        ... on NotionBlock_code {
           id
           type
         }
