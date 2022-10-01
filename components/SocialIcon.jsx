0b4c8fcbc32c6995a429a4e1e3e00337c24ee860
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2022-10-01 18:18:01 -0400

    version control public folder

--- components/SocialIcon.jsx
+++ components/SocialIcon.jsx
@@ -1,6 +1,6 @@
-import Instagram from '../public/social/Platform=InstagramIcon.svg';
-import Polywork from '../public/social/Platform=PolyworkIcon.svg';
-import LinkedIn from '../public/social/Platform=LinkedinIcon.svg';
+import Instagram from '../public/social/InstagramIcon.svg';
+import Polywork from '../public/social/PolyworkIcon.svg';
+import LinkedIn from '../public/social/LinkedinIcon.svg';
 import Styles from './styles/SocialIcon.module.scss';
 
 export default function SocialIcon({ href, icon }) {
